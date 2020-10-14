0ed5766745001fda4d61cd47de46687a0de4bb5d
mgnedash <gnedasch2011@yandex.ru>
2020-10-14 13:04:36 +0300

    fix

--- frontend/modules/site/controllers/DefaultController.php
+++ frontend/modules/site/controllers/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\modules\site\controllers;
 
+use frontend\modules\category\model\Category;
 use frontend\modules\page\model\Page;
 use frontend\modules\url\components\ControllerWithParam;
 use Yii;
@@ -12,7 +13,18 @@ class DefaultController extends ControllerWithParam
 {
     public function actionMainPage()
     {
+        $this->view->title = "Загадки с ответами | 100500 загадок на Pro-Zagadki.ru";
+        $this->view->registerMetaTag(
+            ['name' => 'description', 'content' => "Лучшие загадки с ответами для детей и взрослых! Более 100500 загадок на разные темы на Pro-Zagadki.ru"]
+        );
+
+        $urlsCategorys = Category::find()
+            ->asArray()
+            ->all();
+
+
         return $this->render('MainPage', [
+            'urlsCategoryArr' => $urlsCategorys,
         ]);
     }
 
--- frontend/themes/default/modules/site/views/default/MainPage.php
+++ frontend/themes/default/modules/site/views/default/MainPage.php
@@ -1,1 +1,15 @@
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur, autem cupiditate delectus doloribus impedit laboriosam maiores minima molestias mollitia nihil odit officiis omnis quisquam reiciendis similique ut, vitae, voluptatum.</p>
+<h1>Загадки</h1>
+<ul>
+    <?php foreach($urlsCategoryArr as $url):?>
+            <li><a href="/<?= $url['name_transliteration'];?>"><?= $url['name'] ;?></a></li>
+    <?php endforeach;?>
+</ul>
+
+<style>
+
+    ul > li {
+        display: inline; /* Отображать как строчный элемент */
+        margin-right: 5px; /* Отступ слева */
+        padding: 3px; /* Поля вокруг текста */
+    }
+</style>
